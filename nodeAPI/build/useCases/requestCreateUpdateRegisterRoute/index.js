dcf324ee226101ee2d1fb6bad6a0370f36314e17
MarcoVercosa <marco2007sky@hotmail.com>
2022-06-10 14:38:38 -0300

    Documentation

--- nodeAPI/build/useCases/requestCreateUpdateRegisterRoute/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requestCreateUpdateRegisterController = void 0;
-const requestCreateUpdateRegisterUseCase_1 = require("./requestCreateUpdateRegisterUseCase");
-const requestCreateUpdateRegisterController_1 = require("./requestCreateUpdateRegisterController");
-const requestLCreateUpdateRegisterUseCase = new requestCreateUpdateRegisterUseCase_1.RequestCreateUpdateRegisterUseCase();
-const requestCreateUpdateRegisterController = new requestCreateUpdateRegisterController_1.RequestCreateUpdateRegisterController(requestLCreateUpdateRegisterUseCase);
-exports.requestCreateUpdateRegisterController = requestCreateUpdateRegisterController;
-//# sourceMappingURL=index.js.map
--- nodeAPI/src/useCases/requestCancelRegisterRoute/index.ts
+++
@@ -1,9 +0,0 @@
-//import { RequestCancelActiveRegisterRepository } from "../../repositories/requestCancelActiveRegisterRoute/requestCancelActiveRegisterRepository";
-import { RequestCancelRegisterUseCase } from "./requestCancelRegisterUseCase"
-import { RequestCancelRegisterController } from "./requestCancelRegisterController";
-
-//const requestCancelActiveRegisterRepository = new RequestCancelActiveRegisterRepository()
-const requestLCancelRegisterUseCase = new RequestCancelRegisterUseCase()
-const requestCancelRegisterController = new RequestCancelRegisterController(requestLCancelRegisterUseCase)
-
-export { requestCancelRegisterController }
