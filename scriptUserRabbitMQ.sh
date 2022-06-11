49089272529bc0900b125778ec55e79bbd5b83d4
MarcoVercosa <marco2007sky@hotmail.com>
2022-06-11 13:46:53 -0300

    configure structure

--- scriptUserRabbitMQ.sh
+++ scriptUserRabbitMQ.sh
@@ -1,16 +1,49 @@
 #!/bin/bash
 if ping -c 1 -W 3 172.20.0.3; then
-  echo "COMMANDS EXECUTE ON >>>>>>>>> RABBITMQ SERVER <<<<<<<<<<<<<"
+  echo "###################################################################"
+  echo "#                                                                 #"
+  echo "#                                                                 #"
+  echo "#  COMMANDS EXECUTE ON >>>>>>>>> RABBITMQ SERVER <<<<<<<<<<<<<    #"
+  echo "#                                                                 #"
+  echo "#                                                                 #"
+  echo "###################################################################"
   SERVER=rabbitmq    
 elif ping -c 1 -W 3 172.21.0.3; then
-  echo "COMMANDS EXECUTE ON >>>>>>>>> DEV-RABBITMQ SERVER <<<<<<<<<<<<<"
+  echo "###################################################################"
+  echo "#                                                                 #"
+  echo "#                                                                 #"
+  echo "#  COMMANDS EXECUTE ON >>>>>>>>> DEV-RABBITMQ SERVER <<<<<<<<<<   #"
+  echo "#                                                                 #"
+  echo "#                                                                 #"
+  echo "###################################################################"
   SERVER=dev-rabbitmq
 elif ping -c 1 -W 3 172.22.0.3; then
-  echo "COMMANDS EXECUTE ON >>>>>>>>> 'TEST' DEV-RABBITMQ SERVER <<<<<<<<<<<<"
+  echo "###################################################################"
+  echo "#                                                                 #"
+  echo "#                                                                 #"
+  echo "#  COMMANDS EXECUTE ON >>>>>>>>> 'TEST' DEV-RABBITMQ SERVER <<<<< #"
+  echo "#                                                                 #"
+  echo "#                                                                 #"
+  echo "###################################################################"
   SERVER=dev-rabbitmq 
 else
-    echo "RABBITMQ PROD AND DEV >>>>>>>>> NOT ACESSIBLE <<<<<<<<<<<< "
+  echo "###################################################################"
+  echo "#         E R R O R         E R R O R       E R R O R             #"
+  echo "#                                                                 #"
+  echo "#  RABBITMQ PROD AND DEV >>>>>>>>> NOT ACESSIBLE <<<<<<<<<<<< #"
+  echo "#                                                                 #"
+  echo "#                                                                 #"
+  echo "###################################################################"
 fi
+
+ echo "CHANGING PERMISSIONS /var/lib/rabbitmq/.erlang.cookie"
+    sudo docker container exec $SERVER chmod 600 /var/lib/rabbitmq/.erlang.cookie
+
+
+echo 'WAITING FOR INITIALIZATION RABBITMQ SERVER' 
+sleep 25 &&
+   
+    echo "CONFIGURING USERS, PASS AND QUEUES"
 
     sudo docker container exec $SERVER rabbitmqctl add_user cancel_register_bd cancel_register_bd
     sudo docker container exec $SERVER rabbitmqctl set_user_tags cancel_register_bd  administrator
@@ -31,3 +64,5 @@ fi
     sudo docker container exec $SERVER rabbitmqctl add_user dead_cancel_queue dead_cancel_queue
     sudo docker container exec $SERVER rabbitmqctl set_user_tags dead_cancel_queue administrator 
     sudo docker container exec $SERVER rabbitmqctl set_permissions -p / dead_cancel_queue ".*" ".*" ".*"
+
+echo 'END OF SCRIPT'
